0ac9bff7edad24fe0b5774a9b3badd193bf4f682
AttackPenguin <denis.e.hudon@gmail.com>
2022-02-24 21:42:38 -0500

    sort of working now...

--- programming_asst_1/chat_client2.py
+++ programming_asst_1/chat_client2.py
@@ -1,10 +1,9 @@
 import fcntl
 import os
+import select
 import selectors
 import socket
 import sys
-
-sel = selectors.DefaultSelector()
 
 
 def print_options():
@@ -15,9 +14,8 @@ def print_options():
 
 
 # Get initial connection information from user and attempt connection:
-connected = False
 sock = None
-while not connected:
+while "The Stars Still Shine":
     hostname = input(
         "Please enter the server hostname or IPv4 address: "
     )
@@ -39,23 +37,32 @@ while not connected:
     if response != "HELLO\n":
         print(f"Client did not correctly respond to handshake.\n"
               f"Client response: {response}")
-        sys.exit(0)
+        sys.exit(1)
     sock.sendall(f"AUTH:{user_name}:{password}\n".encode())
     response = sock.recv(1024).decode()
-    if response != 'AUTHYES\n':
+    if response == "UNIQNO\n":
+        print(f"This user is already logged into the server.")
+        continue
+    elif response == 'AUTHNO\n':
         print("Incorrect username and/or password.")
-    else:
-        response = sock.recv(1024)
+        continue
+    elif response == 'AUTHYES\n':
         print("You are now authenticated.")
         print_options()
-        connected = True
+        break
+    else:
+        print(f"Unhandled response received: {response}")
+        sys.exit(1)
 
 
 def process_response(response: str):
-    if response.startswith('SIGNIN'):
-        print(f"\n{response[8:-1]} Signed In\n")
+    if response is None or response == '':
+        pass
+    elif response.startswith('SIGNIN'):
+        if not response[7:-1] == user_name:
+            print(f"\n{response[7:-1]} Signed In\n")
     elif response.startswith('SIGNOFF'):
-        print(f"\n{response[9:-1]} Signed Out\n")
+        print(f"\n{response[8:-1]} Signed Out\n")
     elif response.startswith('From'):
         subs = response.split(':')
         print(f"\nMessage from {subs[1]}: {subs[2][0:-1]}\n")
@@ -64,7 +71,7 @@ def process_response(response: str):
               f"{response}\n")
 
 
-def get_server_message(sock):
+def get_server_message():
     response = sock.recv(1024).decode()
     process_response(response)
 
@@ -72,19 +79,17 @@ def get_server_message(sock):
 continue_listening = True
 
 
-def get_user_input(user_input):
-    user_input = user_input.read()
-    print(user_input)
+def get_user_input():
+    user_input = sys.stdin.readline()
     if user_input == '1\n':
         # Get and print list of online users
         sock.sendall("LIST\n".encode())
         while True:
             response = sock.recv(1024).decode()
-            print(response)
             if (
-                response.startswith('SIGNIN') or
-                response.startswith('SIGNOFF') or
-                response.startswith('From')
+                    response.startswith('SIGNIN') or
+                    response.startswith('SIGNOFF') or
+                    response.startswith('From')
             ):
                 process_response(response)
             else:
@@ -93,45 +98,38 @@ def get_user_input(user_input):
                 for user in users:
                     print(f'  {user}')
                 break
-        print_options()
-    if user_input == '2\n':
-        sel.unregister(sock)
-        sel.unregister(sys.stdin)
+    elif user_input == '2\n':
+
         print("Enter user you would like to message:")
-        user = input()
+        user = sys.stdin.readline()
         print('Message:')
-        message = input()
+        message = sys.stdin.readline()
         sock.sendall(
-            f"To:{user}:{message}"
+            f"To:{user}:{message}".encode()
         )
         print("Message sent.\n")
-        print_options()
-        sel.register(sock, selectors.EVENT_READ, get_server_message)
-        sel.register(sock, selectors.EVENT_READ, get_server_message)
-    if user_input == '3\n':
+
+    elif user_input == '3\n':
         print('Signing off...')
-        sock.send('BYE'.encode())
-        sel.unregister(sock)
-        sel.unregister(sys.stdin)
+        sock.sendall('BYE\n'.encode())
         sock.close()
         sys.exit(0)
     else:
         print("Input not recognized.\n")
-        print_options()
+    print_options()
 
-
-sel.register(sock, selectors.EVENT_READ, get_server_message)
-sel.register(sys.stdin, selectors.EVENT_READ, get_user_input)
 
 # I cannot figure out how to do this with os.set_blocking, and I'm tired,
 # so I'm just accessing the nix functionality via fcntl. Not great for
 # portability, I know.
-fcntl.fcntl(
-    sys.stdin, fcntl.F_SETFL,
-    fcntl.fcntl(sys.stdin, fcntl.F_GETFL) | os.O_NONBLOCK
-)
+# fcntl.fcntl(
+#     sys.stdin, fcntl.F_SETFL,
+#     fcntl.fcntl(sys.stdin, fcntl.F_GETFL) | os.O_NONBLOCK
+# )
 
-while continue_listening:
-    for key, mask in sel.select():
-        callback = key.data
-        callback(key.fileobj)
+while "The Wind Still Blows":
+    readable, _, _ = select.select([sock, sys.stdin], [], [])
+    if sys.stdin in readable:
+        get_user_input()
+    if sock in readable:
+        get_server_message()
